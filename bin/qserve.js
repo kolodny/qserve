c1ee806724502ceeccdaae839f92ef2a1f71d91a
Moshe Kolodny <kolodny@github.com>
2014-12-14 21:24:56 -0500

    intial commit

---
+++ bin/qserve.js
@@ -0,0 +1,16 @@
+var qserve = require('../lib');
+var resolve = require('path').resolve
+var program = require('commander');
+
+program
+  .version(require('../package.json').version)
+  .usage('[options] [dir]')
+  .option('-p, --port <port>', 'specify the port [3000]', Number, 3000)
+  .parse(process.argv);
+
+var path = resolve(program.args.shift() || '.');
+
+qserve({
+  port: program.port,
+  path: path
+});
