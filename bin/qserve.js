10000286ea5aea98222f969f74c907ac81cf7e2a
Moshe Kolodny <kolodny@github.com>
2014-12-14 22:00:39 -0500

    return the server object and added a silent cmd option

--- bin/qserve.js
+++ bin/qserve.js
@@ -8,11 +8,13 @@ program
   .version(require('../package.json').version)
   .usage('[options] [dir]')
   .option('-p, --port <port>', 'specify the port [3000]', Number, 3000)
+  .option('-s, --silent', 'hide logging info')
   .parse(process.argv);
 
 var path = resolve(program.args.shift() || '.');
 
 qserve({
   port: program.port,
-  path: path
+  path: path,
+  debug: !program.silent
 });
